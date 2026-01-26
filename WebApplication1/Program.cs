504d4a0971571ac05105bdb9a8b3dc61645f7b6d
Enock <tshims.enock31@gmail.com>
2026-01-25 22:32:39 -0500

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+//Routing
+
+// "/shirts"
+app.MapGet("/shirts", () =>
+{
+    return "Reading all the shirts";
+});
+
+app.MapGet("/shirts/{id}", (int id) =>
+{
+    return $"Reading shirt with ID: {id}";
+});
+
+app.MapPost("/shirts", () =>
+{
+    return $"Creating a shirt";
+});
+
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"updating shirt: {id}";
+});
+
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting short: {id}";
+});
+
+app.Run();
