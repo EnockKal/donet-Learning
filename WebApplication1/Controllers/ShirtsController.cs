9a2a18e64d2c1793257682a006f8012750cde205
Enock <tshims.enock31@gmail.com>
2026-01-27 01:56:42 -0500

    In memory repository

--- WebApplication1/Controllers/ShirtsController.cs
+++ WebApplication1/Controllers/ShirtsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models;
+using WebApplication1.Models.Repository;
 
 namespace WebApplication1.Controllers
 {
@@ -7,14 +8,6 @@ namespace WebApplication1.Controllers
     [Route("api/[controller]")] // auto uses controller name
     public class ShirtsController : ControllerBase
     {
-        private List<Shirt> shirts = new List<Shirt>()
-        {
-            new Shirt {ShirtId = 1, Brand = "My Band", Color = "Blue", Gender = "Male", Price = 30, Size = 10 },
-            new Shirt {ShirtId = 2, Brand = "My Band", Color = "Black", Gender = "Male", Price = 35, Size = 12 },
-            new Shirt {ShirtId = 3, Brand = "Your Band", Color = "Pink", Gender = "woMale", Price = 28, Size = 8 },
-            new Shirt {ShirtId = 4, Brand = "Your Band", Color = "Yellow", Gender = "woMale", Price = 30, Size = 9 }
-        };
-
         [HttpGet] // maps to GET request
         public IActionResult GetShirt()
         {
@@ -27,7 +20,7 @@ namespace WebApplication1.Controllers
             if (id <= 0)
                 return BadRequest();
 
-            var shirt = shirts.First(x => x.ShirtId == id);
+            var shirt = ShirtsRepository.GetShirtById;
             if (shirt == null)
                 return NotFound();
 
---
+++ WebApplication1/Models/Repository/ShirtsRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApplication1.Models.Repository
+{
+    public static class ShirtsRepository
+    {
+        private static List<Shirt> shirts = new List<Shirt>()
+        {
+            new Shirt {ShirtId = 1, Brand = "My Band", Color = "Blue", Gender = "Male", Price = 30, Size = 10 },
+            new Shirt {ShirtId = 2, Brand = "My Band", Color = "Black", Gender = "Male", Price = 35, Size = 12 },
+            new Shirt {ShirtId = 3, Brand = "Your Band", Color = "Pink", Gender = "woMale", Price = 28, Size = 8 },
+            new Shirt {ShirtId = 4, Brand = "Your Band", Color = "Yellow", Gender = "woMale", Price = 30, Size = 9 }
+        };
+
+        public static bool ShirtExists(int id)
+        {
+            return shirts.Any(x => x.ShirtId == id);
+        }
+
+        public static Shirt? GetShirtById(int id)
+        {
+            return shirts.FirstOrDefault(x => x.ShirtId == id);
+        }
+    }
+}
