bf97e117b6cee5376c84c13435e5b6c1bc9471d2
Enock <tshims.enock31@gmail.com>
2026-01-27 01:49:01 -0500

    Web API Return Types. using IActionResult

--- WebApplication1/Controllers/ShirtsController.cs
+++ WebApplication1/Controllers/ShirtsController.cs
@@ -7,16 +7,31 @@ namespace WebApplication1.Controllers
     [Route("api/[controller]")] // auto uses controller name
     public class ShirtsController : ControllerBase
     {
-        [HttpGet] // maps to GET request
-        public string GetShirt()
+        private List<Shirt> shirts = new List<Shirt>()
         {
-            return "Reading all shirts";
+            new Shirt {ShirtId = 1, Brand = "My Band", Color = "Blue", Gender = "Male", Price = 30, Size = 10 },
+            new Shirt {ShirtId = 2, Brand = "My Band", Color = "Black", Gender = "Male", Price = 35, Size = 12 },
+            new Shirt {ShirtId = 3, Brand = "Your Band", Color = "Pink", Gender = "woMale", Price = 28, Size = 8 },
+            new Shirt {ShirtId = 4, Brand = "Your Band", Color = "Yellow", Gender = "woMale", Price = 30, Size = 9 }
+        };
+
+        [HttpGet] // maps to GET request
+        public IActionResult GetShirt()
+        {
+            return Ok("Reading all shirts");
         }
 
         [HttpGet("{id}")]
-        public string GetShirtById(int id)
+        public IActionResult GetShirtById(int id) // using IActionResult as return type to simplify dev process and good for Unit Testing,...
         {
-            return $"Reading all shirts: {id}";
+            if (id <= 0)
+                return BadRequest();
+
+            var shirt = shirts.First(x => x.ShirtId == id);
+            if (shirt == null)
+                return NotFound();
+
+            return Ok(shirt);
         }
 
         //[HttpPost]
@@ -26,15 +41,15 @@ namespace WebApplication1.Controllers
         //}
 
         [HttpPut("{id}")]
-        public string UpdateShirt(int id)
+        public IActionResult UpdateShirt(int id)
         {
-            return $"Updating shirt: {id}";
+            return Ok($"Updating shirt: {id}");
         }
 
         [HttpDelete("{id}")]
-        public string DeletetingShirt(int id)
+        public IActionResult DeletetingShirt(int id)
         {
-            return $"Updating shirt: {id}";
+            return Ok($"Updating shirt: {id}");
         }
 
         // Modeling Binding:
@@ -48,15 +63,15 @@ namespace WebApplication1.Controllers
          */
 
         [HttpGet("{id}/{color}")]
-        public string GetShirtByIdAndColor(int id, [FromBody] string color)
+        public IActionResult GetShirtByIdAndColor(int id, [FromBody] string color)
         {
-            return $"Reading all shirts: {id}";
+            return Ok($"Reading all shirts: {id}");
         }
 
         [HttpPost]
-        public string CreateShirtFromBody([FromBody] Shirt shirt)
+        public IActionResult CreateShirtFromBody([FromBody] Shirt shirt)
         {
-            return $"Creating shirt";
+            return Ok($"Creating shirt");
         }
     }
 }
