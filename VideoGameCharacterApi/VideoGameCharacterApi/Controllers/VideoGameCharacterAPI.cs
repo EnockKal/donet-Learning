d850f2f4ad4b497ac1a1881deeaed915890c81f2
Enock <tshims.enock31@gmail.com>
2026-01-31 20:15:38 -0500

    Staring with Controller (Get end point) and adding and using Scalar tool instead of Swagger

---
+++ VideoGameCharacterApi/VideoGameCharacterApi/Controllers/VideoGameCharacterAPI.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameCharacterAPI : ControllerBase
+    {
+        // Temporary Data to work with
+        static List<Character> character = new List<Character>
+        {
+            new Models.Character { Id = 1, Name = "Mario", Game = "Super Mario Bros", Role = "Hero"},
+            new Models.Character { Id = 2, Name = "Link", Game = "Super Mario Bros", Role = "Hero"},
+            new Models.Character { Id = 3, Name = "Boweser", Game = "Super Mario Bros", Role = "Hero"}
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<Character>> GetCharacters()
+        {
+            return Ok(character);
+        }
+    }
+}
--- VideoGameCharacterApi/VideoGameCharacterApi/Program.cs
+++ VideoGameCharacterApi/VideoGameCharacterApi/Program.cs
@@ -1,20 +1,20 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-// Swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference(); // I'm Using Scalar for this prject.
 }
 
 app.UseHttpsRedirection();
